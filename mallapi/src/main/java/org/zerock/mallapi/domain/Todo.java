14fabdf88295e969d93cdd65ea0c642fde772994
koratoo <ssyk2@naver.com>
2023-12-10 12:06:00 +0900

    Todo Entity 자바 파일 작성

---
+++ mallapi/src/main/java/org/zerock/mallapi/domain/Todo.java
@@ -0,0 +1,32 @@
+package org.zerock.mallapi.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    private String title;
+
+    private String writer;
+
+    private boolean complete;
+
+    private LocalDate dueDate;
+}
