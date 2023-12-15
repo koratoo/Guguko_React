70819a53f98da9ce6c53d8dbe5d7b0d69a01286e
koratoo <ssyk2@naver.com>
2023-12-16 06:24:24 +0900

    상단메뉴 컴포넌트 구성

---
+++ src/components/menus/BasicMenu.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const BasicMenu = () => {
+    return (
+        <nav id='navbar' className=" flex bg-blue-300">
+            <div className="w-4/5 bg-gray-500" >
+                <ul className="flex p-4 text-white font-bold">
+                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
