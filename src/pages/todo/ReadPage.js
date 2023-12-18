11032dd474f87a12cf447114a045e35118704f3c
koratoo <ssyk2@naver.com>
2023-12-18 19:44:54 +0900

    AddPage추가 수정페이지로 이동 로직 추가

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,12 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useCallback } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ReadPage = () => {
 
     const {tno} = useParams()
 
+    const navigate = useNavigate()
+
+    const moveToModify = useCallback((tno) => {
+        navigate({pathname:`/todo/modify/${tno}`})
+    },[tno])
+
     return (
         <div className="text-3xl font-extrabold">
             Todo Read Page Component {tno}
+
+            <div>
+                <button onClick={() => moveToModify(33)}>Test Modify</button>
+            </div>
         </div>
     );
 }
